21be1fdd20bb25e68f44253f0cf20cfd1c87b27c
duttaani <duttaaniruddha31@gmail.com>
2022-04-30 16:25:02 +0530

    Feat: Added signin

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home = () => {
+  return <div>home</div>
+};
+
+
+export default Home;
